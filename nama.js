4b6f4f8a2fae6c87beea439ef1266fc2748cfff0
sorongtechone <93487560+sorongtechone@users.noreply.github.com>
2023-03-15 05:42:18 +0900

    Update nama.js

--- nama.js
+++ nama.js
@@ -1,1 +1,1 @@
-document.documentElement.innerHTML=String.fromCharCode(60 116 105 116 108 101 62 72 65 67 75 69 68 32 66 89 32 77 46 65 32 68 69 83 84 82 79 89 83 81 85 65 68 60 47 116 105 116 108 101 62)
+document.title = "newTitle";
